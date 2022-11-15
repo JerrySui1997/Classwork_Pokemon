97a9cb4b6ec4a934dd0b1ad6e1bfa8d5ad34e26f
Jimmy <suijs@bu.edu>
2022-11-14 21:24:22 -0500

    1.1

---
+++ Point2D.cpp
@@ -0,0 +1,9 @@
+
+class Point2D
+{
+  double x;
+  double y;
+
+public:
+  Point2D(double in_x, double in_y)
+}
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+double GetDistanceBetween(Point2D p1, Point2D p2)
+{
+
+}
+
+int main()
+{
+
+  return 0;
+}
